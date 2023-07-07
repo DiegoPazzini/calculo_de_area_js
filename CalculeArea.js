d5fa819487740c79144e7483d9ec0c52d3119058
diego.pazzini2009@hotmail.com <diegopazzini2009@hotmail.com>
2023-07-07 03:11:28 -0300

    first commit

---
+++ CalculeArea.js
@@ -0,0 +1,9 @@
+let base = prompt ("Insira a base")
+let altura = prompt ("Insira a altura")
+
+function calculaArea(base, altura) { 
+area = base * altura 
+return area; 
+}
+
+alert ("Sua área é de: " + calculaArea(base, altura)) 
